a69e00776b1f84e5973170b65862de7c973c7753
ycv005 <ycverma005@gmail.com>
2019-11-06 16:00:28 +0530

    leetcode prob

---
+++ longSubStringNoRepeat.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = ''
+        mx = 0
+        for c in s:
+            if c not in seen:
+                seen+=c
+            else:
+                seen = seen[seen.index(c) + 1:] + c
+            mx = max(mx, len(seen))
+        return mx
