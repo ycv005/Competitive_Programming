b764f2505006cd62729e291b7cbde2a762e8a743
ycv005 <ycverma005@gmail.com>
2019-12-01 12:02:26 +0530

    longest comn prefix, horiztal traversal

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,32 +1,26 @@
-# https://leetcode.com/problems/longest-common-prefix/submissions/
-def reduce_s(strs,s):
-    while len(s)>0:
-        s=s[:len(s)-1]
-        if s in strs[:len(s)]:
+def reduce_s(s,t):
+    for i in range(len(s)):
+        if s[i]!=t[i]:
+            s=s[:i]
             break
     return s
 
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+class Solution(object):
+    def longestCommonPrefix(self, strs):
         s=""
         if len(strs)==0:
             return s
-        prev_f = len(strs[0])
-        prev_l = len(strs[-1])
-        strs.sort(key= lambda x: len(x))
-        suc_f = len(strs[0])
-        suc_len = len(strs[-1])
-        if prev_l != suc_len or prev_f!=suc_f:
-            s = strs[0]
-            for i in strs:
-                if s not in i[:len(s)]:
-                    s = reduce_s(i,s)
-        elif len(strs)==1:
-            s = strs[0]
-        else:
-#             all of same length
-            s = strs[0]
-            for i in range(1,len(strs)):
-                if s not in strs[i][:len(s)]:
-                    s = reduce_s(strs[i],s)
+        s=strs[0]
+        for i in range(1,len(strs)):
+            if s=="":
+                break
+            t = strs[i][:len(s)]
+            if s != t:
+                # sub match
+                s=s[:len(t)]
+                if s!=t:
+                    # iterate from begining to find out commonPart
+                    s = reduce_s(s,t)
+            else:
+                s=s[:len(t)]
         return s
