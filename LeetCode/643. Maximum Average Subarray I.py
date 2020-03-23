c7f3a62137bc02df554dfef795dc8a3f0143034b
ycv005 <ycverma005@gmail.com>
2020-03-23 15:48:43 +0530

    max, avg., number, array

---
+++ LeetCode/643. Maximum Average Subarray I.py
@@ -0,0 +1,10 @@
+from collections import deque
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        tmp = deque(nums[:k])
+        tmpSum = maxSum =sum(tmp)
+        for i in range(k,len(nums)):
+            tmpSum = tmpSum - tmp.popleft()+ nums[i]
+            tmp.append(nums[i])
+            maxSum = max(maxSum, tmpSum)
+        return maxSum/k
