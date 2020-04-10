f5dc2e413d88e712f32060e41c678209c60c5355
ycv005 <ycverma005@gmail.com>
2020-04-10 16:31:48 +0530

    pascal triangle, index

---
+++ LeetCode/119. Pascal's Triangle II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getRow(self, A: int) -> List[int]:
+        res=[]
+        for i in range(A+1):
+            t=[1]*(i+1)
+            res.append(t)
+        for i in range(1,len(res)):
+            for j in range(len(res[i])):
+                t=s=0
+                if j-1>=0:
+                    t=res[i-1][j-1]
+                if j<len(res[i-1]):
+                    s=res[i-1][j]
+                res[i][j]= s+ t
+        return res[A]
