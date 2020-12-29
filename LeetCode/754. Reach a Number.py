1b24b897f2253308a17b1a616bc3eaa2a0f08e01
Yash Chandra Verma <ycverma005@gmail.com>
2020-12-29 14:05:00 +0530

    natural number, odd, even

---
+++ LeetCode/754. Reach a Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        k = 0
+        while target > 0:
+            k += 1
+            target -= k
+
+        if target % 2 == 0:
+            return k
+        else:
+            # we are ahead of target
+            return k + 1 + k % 2
